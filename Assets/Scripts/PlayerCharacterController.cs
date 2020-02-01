0d2e721d3cd6e27be324e85c69cb76fd12d785fe
Cate Mackenzie <cate.c.mackenzie@gmail.com>
2020-01-31 21:30:06 -0800

    Add walking sfx, converts player to prefab

--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -6,17 +6,23 @@ public class PlayerCharacterController : MonoBehaviour
 {
     [Header("References")]
     public Camera playerCamera;
+    public AudioSource audioSource;
 
     [Header("Movement")]
     public float maxGroundSpeed = 10f;
     public float groundMovementSharpness = 15f;
     public float rotationSpeed = 1f;
 
+    [Header("Audio")]
+    public float footstepSFXFrequency = 1f;
+    public AudioClip footstepSFX;
+
     public Vector3 characterVelocity { get; set; }
 
     CharacterController m_CharacterController;
     PlayerInputHandler m_InputHandler;
     float m_VerticalCameraAngle = 0f;
+    float m_FootstepDistanceCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -41,5 +47,14 @@ public class PlayerCharacterController : MonoBehaviour
         Vector3 targetVelocity = worldSpaceMoveInput * maxGroundSpeed;
         characterVelocity = Vector3.Lerp(characterVelocity, targetVelocity, groundMovementSharpness * Time.deltaTime);
         m_CharacterController.Move(characterVelocity * Time.deltaTime);
+
+        // Footstep SFX
+        if (m_FootstepDistanceCounter >= 1f / footstepSFXFrequency)
+        {
+            m_FootstepDistanceCounter = 0f;
+            audioSource.PlayOneShot(footstepSFX);
+        }
+
+        m_FootstepDistanceCounter += characterVelocity.magnitude * Time.deltaTime;
     }
 }
