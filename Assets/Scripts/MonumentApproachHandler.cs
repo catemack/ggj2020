c3adfff10bdc9f0662af3a3f32db8c1af1b9960e
Cate Mackenzie <cate.c.mackenzie@gmail.com>
2020-02-01 11:37:48 -0800

    Adds WIP script to detect if the player is looking at an object within a certain distance

---
+++ Assets/Scripts/MonumentApproachHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonumentApproachHandler : MonoBehaviour
+{
+    public float minDistance = 10f;
+    public Camera playerCamera;
+
+    BoxCollider m_BoxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_BoxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
+        {
+            if (hit.distance <= minDistance)
+            {
+                //Debug.Log("hit");
+            }
+        }
+    }
+}
