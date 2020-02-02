cc145b856534f1f0c7ba12c763fe21e3315e65f5
Cate Mackenzie <cate.c.mackenzie@gmail.com>
2020-02-02 11:48:57 -0800

    Adds script to rotate objects randomly

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+
+    Vector3 rotationSpeedVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float xRotation = Random.Range(0, rotationSpeed);
+        float yRotation = Random.Range(0, rotationSpeed - xRotation);
+        float zRotation = Random.Range(0, rotationSpeed - xRotation - yRotation);
+        rotationSpeedVec = new Vector3(xRotation, yRotation, zRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeedVec * Time.deltaTime);
+    }
+}
