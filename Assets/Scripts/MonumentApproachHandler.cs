f99396030344bb11c957d63ca2f6e5013a56e2d1
Cate Mackenzie <cate.c.mackenzie@gmail.com>
2020-02-02 15:13:04 -0800

    Fixes game progression breaking due to destroyed monolith

--- Assets/Scripts/MonumentApproachHandler.cs
+++ Assets/Scripts/MonumentApproachHandler.cs
@@ -10,7 +10,6 @@ public class MonumentApproachHandler : MonoBehaviour
     public AudioSource playerAudio;
     public AudioClip crumbleSFX;
     public AudioClip badNoise;
-    public GameObject wholeMonolith;
     public GameObject brokenMonolith;
     public float destructionTimer = 10f;
 
@@ -33,43 +32,40 @@ public class MonumentApproachHandler : MonoBehaviour
         RaycastHit hit;
 
         // Check if the player is looking at the object within a certain range
-        if (!GameComplete() && m_Monolith && Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
+        if (m_Monolith.activeSelf && Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
         {
             if (hit.transform.parent && hit.transform.parent.name == "Monolith and Platform" && hit.distance <= minDistance)
             {
                 lookTime += Time.deltaTime;
 
-                if (lookTime >= destructionTimer || m_InputHandler.GetInteractInputDown())
+                if (lookTime >= destructionTimer || (m_InputHandler.GetInteractInputDown() && !GameComplete()))
                 {
-                    Vector3 monolithPosition = m_Monolith.transform.position + new Vector3(-0.6f, 0f, -0.2f);
+                    if (GameComplete())
+                    {
+                        playerAudio.PlayOneShot(badNoise);
+                        Application.Quit();
+                    }
+                    else
+                    {
+                        Vector3 monolithPosition = m_Monolith.transform.position + new Vector3(-0.6f, 0f, -0.2f);
 
-                    Destroy(m_Monolith);
-                    Instantiate(brokenMonolith, monolithPosition, Quaternion.identity, gameObject.transform);
+                        //Destroy(m_Monolith);
+                        m_Monolith.SetActive(false);
+                        Instantiate(brokenMonolith, monolithPosition, Quaternion.identity, gameObject.transform);
 
-                    playerAudio.PlayOneShot(badNoise);
-                    playerAudio.PlayOneShot(crumbleSFX);
+                        playerAudio.PlayOneShot(badNoise);
+                        playerAudio.PlayOneShot(crumbleSFX);
 
-                    lookTime = 0f;
+                        lookTime = 0f;
+                    }
                 }
             }
         }
-        else if (GameComplete() && !m_Monolith)
+        else if (GameComplete() && !m_Monolith.activeSelf)
         {
-            m_Monolith = Instantiate(wholeMonolith, transform);
+            //m_Monolith = Instantiate(wholeMonolith, transform);
+            m_Monolith.SetActive(true);
             playerAudio.PlayOneShot(badNoise);
-        }
-        else if (GameComplete() && m_Monolith && Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
-        {
-            if (hit.transform.parent && hit.transform.parent.name == "Monolith and Platform" && hit.distance <= minDistance)
-            {
-                lookTime += Time.deltaTime;
-
-                if (lookTime >= destructionTimer)
-                {
-                    playerAudio.PlayOneShot(badNoise);
-                    Application.Quit();
-                }
-            }
         }
     }
 
